5a0068d7e6248ee816bd3a54794fc8b7027bc495
Santush Deb Nath <santush.lu.bd@gmail.com>
2024-07-04 02:58:51 +0100

    Update useEditorOptions.ts

--- src/hooks/useEditorOptions.ts
+++ src/hooks/useEditorOptions.ts
@@ -1,6 +1,6 @@
 import {MailUiEditorProps} from "../MailUiEditor";
 import {useMemo} from "react";
-// import pkg from "../../package.json";
+import pkg from "../../package.json";
 
 const useEditorOptions = (props: MailUiEditorProps, editorId: string) => {
     return useMemo(() => {
@@ -12,10 +12,10 @@ const useEditorOptions = (props: MailUiEditorProps, editorId: string) => {
             locale: props.options?.locale,
             tools: props.options?.tools,
             id: editorId,
-            // source: {
-            //     name: pkg.name,
-            //     version: pkg.version,
-            // },
+            source: {
+                name: pkg.name,
+                version: pkg.version,
+            },
         };
         return options;
     }, [props.options, editorId]);
